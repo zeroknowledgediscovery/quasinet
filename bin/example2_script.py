8af3f5380436928b78688e3761fbc058f6a6eb62
timmyli25 <timmyli530@gmail.com>
2019-07-04 15:25:44 -0500

    updated example2 script

--- bin/example2_script.py
+++ bin/example2_script.py
@@ -1,4 +1,5 @@
 from quasinet import Qnet
+import subprocess
 
 responses = list(range(0,400))
 
@@ -15,3 +16,6 @@ responses = list(range(0,400))
 #Step 4:
 #Qnet.connectQnet(responses, 0.75, '75network.dot','75network.dat',tree_dir='tree/',DEBUG=True)
 #Qnet.draw_Qnet('75network.dot',out_name = '75net.png')
+
+#Step 5:
+#subprocess.Popen(["dot", '-Tpng', 'tree/P25.dot', '-o', 'decision_tree25.png'])
