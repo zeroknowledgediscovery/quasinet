996a03f4058850a6ea75153a31a8a4b0ea9e30a5
JinLi711 <jinli7255@gmail.com>
2020-09-11 10:33:02 -0500

    added extensions

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,11 @@ author = 'ZeD'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
