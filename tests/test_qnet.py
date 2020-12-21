0a2709eeae740cbadacbdbd60af6e5beb01ee31d
JinLi711 <jinli7255@gmail.com>
2020-12-21 07:43:33 -0600

    moved some tests from `test_qnet` to `test_tree`

--- tests/test_qnet.py
+++ tests/test_qnet.py
@@ -2,29 +2,7 @@ import unittest
 
 import pandas as pd
 
-from quasinet.tree import Node, get_nodes
 from quasinet.qnet import Qnet, qdistance, membership_degree
-
-@unittest.skip('Correct')
-class TestTree(unittest.TestCase):
-
-    @staticmethod
-    def initialize_node():
-        root = Node(1) 
-        root.left = Node(2) 
-        root.right = Node(3) 
-        root.left.left = Node(4) 
-        root.left.right = Node(5) 
-
-        return root
-
-    def test_get_nodes(self):
-        root = self.initialize_node()
-        nodes = get_nodes(root)
-        all_cols = set([node.col for node in nodes])
-        self.assertEqual(
-            all_cols,
-            set(range(1, 6)))
 
 class TestQnet(unittest.TestCase):
     DATA_DIR = 'examples/example_data/'
@@ -48,7 +26,6 @@ class TestQnet(unittest.TestCase):
 
         return self.myqnet
 
-    # @unittest.skip('Correct')
     def test_qdistance(self):
         X = self.load_data()
 
@@ -58,13 +35,12 @@ class TestQnet(unittest.TestCase):
         qdist = qdistance(seq1, seq2, myqnet, myqnet) 
         self.assertGreaterEqual(qdist, 0)
 
-    # @unittest.skip('Correct')
     def test_membership_degree(self):
         X = self.load_data()
         seq1 = X[1]
         myqnet = self.test_create_qnet()
         mem_degree = membership_degree(seq1, myqnet)
 
-# unittest.main()     
+ 
 if __name__ == "__main__":
     unittest.main()
