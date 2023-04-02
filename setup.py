362c115f49fd0c7b55b20f00e6f91e8b68734953
ishanuc <ishanu.chattopadhyay@gmail.com>
2023-04-02 11:57:13 -0500

    1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class CustomInstall(install):
             if platform == "darwin":
                 import shutil
                 print('copying dcor for mac')
-                shutil.copy(install_path+'/bin/dcor.so.mac',
+                shutil.copy(install_path+'/bin/macnew_dcor.so',
                             install_path+'/bin/dcor.so')
         atexit.register(_post_install)
         install.run(self)
