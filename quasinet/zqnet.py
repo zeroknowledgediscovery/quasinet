1e2ce0b644c8a0d7c0d32979b90cc80c8c3def2d
ishanuc <ishanu.chattopadhyay@gmail.com>
2023-08-19 04:37:40 -0500

    1

--- quasinet/zqnet.py
+++ quasinet/zqnet.py
@@ -1,6 +1,56 @@
 from quasinet.qnet import qdistance_matrix, Qnet
 import numpy as np
 
+
+
+def extract_diagonal_blocks(M, L):
+    blocks = []
+    start_row = 0
+    start_col = 0
+
+    for l in L:
+        block = M[start_row:start_row+l, start_col:start_col+l]
+        blocks.append(block)
+        
+        start_row += l
+        start_col += l
+
+    return blocks
+
+
+import json
+import subprocess
+def remove_suffix(s):
+    if s.endswith('.H'):
+        return s[:-2]
+    elif s.endswith('.'):
+        return s[:-1]
+    return s
+
+def get_description_curl(code):
+    code=remove_suffix(code)
+    url = f"http://icd10api.com/?code={code}&desc=short&r=json"
+    #url = f"https://clinicaltables.nlm.nih.gov/api/icd10cm/v3/search?sf=code,name&terms={code}&maxList=1"
+    response = subprocess.run(["curl", "-s", url], capture_output=True, text=True)
+    data = json.loads(response.stdout)
+    #print(data)
+    #print(data.keys())
+    if data['Response']:
+        return data['Description']
+    else:
+        return code
+
+
+def replace_with_d(S, j, d):
+    d_array = np.array(d)[:, np.newaxis]
+    output = np.tile(S, (len(d), 1))
+    output[:, j] = d_array[:, 0]
+    return output
+
+
+
+
+
 class zQnet(Qnet):
     """Extended Qnet architecture (`zQnet`).
 
@@ -116,3 +166,65 @@ class zQnet(Qnet):
             content = f.read()
             self.description = content
         return content
+
+
+    def zshap(self,seq=None,m=35):
+        """
+        A superfast approximation of SHAP for zQnet
+
+        Parameters
+        ----------
+        seq : numpy array of str
+            The sequence around which we are evaluating perturbations. 
+            By default it is the array oif empty strings, which represents average behavior
+        m : int
+            Length of shap return dataframe
+        Returns
+        -------
+        pandas.DataFrame
+            dataframe with shapo values and index mapped to short description of icd10 codes
+        """
+        
+        if seq is None:
+            seq=np.array(['']*len(self.feature_names))
+        pdist=model.predict_distributions(seq)
+        L=[len(pdist[i].keys()) for i in range(len(pdist))]
+        A=[]
+        for index_ in range(len(seq)):
+            A.extend(replace_with_d(seq, index_, list(pdist[index_].keys())))
+        D=qdistance_matrix(np.array(A),np.array(A),self,self)
+        Shm=[np.std(m) for m in extract_diagonal_blocks(D, L)]
+        sf=pd.DataFrame(Shm,model.feature_names,columns=['shap']).sort_values('shap').tail(m)
+        sf['code']=sf.index.values
+        sf.index=[get_description_curl(code) for code in sf.index.values]
+        return sf
+
+    def personal_zshap(self,s,eps=1e-7):
+        """
+        A superfast approximation of SHAP for zQnet for individual samples 
+
+        Parameters
+        ----------
+        s : numpy array of str
+            The sequence around which we are evaluating perturbations. 
+        eps : float
+            shap value cutoff
+        Returns
+        -------
+        pandas.DataFrame
+            dataframe with shapo values and index mapped to short description of icd10 codes
+        """
+        
+        rs=self.risk_max(np.array([s]))
+        sD={self.feature_names[i]:rs-self.risk_max(np.array([np.where(np.arange(len(s)) == i,
+                                                                      'H', s)])) for i in np.where(s!='H')[0]}
+        sf=pd.DataFrame(sD,index=['shap']).transpose()
+        sf=sf[sf.abs()>eps].dropna()
+        sf.index=[x+y for (x,y) in zip(sf.index.values,
+                                       s[np.array([np.where(self.feature_names==i)[0][0] for i in sf.index.values])])]
+        sf['code']=sf.index.values
+        sf.index=[get_description_curl(code) for code in sf.index.values]
+        return sf
+
+
+    
